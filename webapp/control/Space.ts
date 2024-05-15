6ba960143c025b6986b14cb73c27e2d819dd0f77
lemaiwo <wouterlemaire120@hotmail.com>
2024-05-15 15:22:03 +0200

    fix

--- webapp/control/Space.ts
+++ webapp/control/Space.ts
@@ -21,37 +21,35 @@ export default class Space extends Control {
     };
     static renderer = SpaceRenderer;
     onAfterRendering() {
-		const numStars = 1000;
-		const mainDiv = this.getDomRef() as HTMLElement;
-
-		// For every star we want to display
-		for (let i = 0; i < numStars; i++) {
-			const { top, left } = this.getRandomPosition(mainDiv);
-			mainDiv.append(this.getRandomStar(top, left));
-		}
-	}
-	getRandomStar(top: string, left: string) {
-		const star = document.createElement("div");
-		star.className = "star";
-		star.style.top = top;
-		star.style.left = left;
-		return star;
-	}
-	getRandomPosition(element: HTMLElement) {
-		return {
-			top: `${this.getRandomNumber(element.offsetHeight||1000)}px`,
-			left: `${this.getRandomNumber(element.offsetWidth)}px`,
-		};
-	}
-	getRandomNumber(value: number) {
-		return Math.floor(Math.random() * value);
-	}
-	setProp1(value: string) {
-		this.setProperty("prop1", value, true);
-		return this;
-	}
-	setSrc1(value: string) {
-		this.setProperty("src1", value, true);
-		return this;
-	}
+        const numStars = 1000;
+        const mainDiv = this.getDomRef() as HTMLElement;
+        for (let i = 0; i < numStars; i++) {
+            const { top, left } = this.getRandomPosition(mainDiv);
+            mainDiv.append(this.getRandomStar(top, left));
+        }
+    }
+    getRandomStar(top: string, left: string) {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.top = top;
+        star.style.left = left;
+        return star;
+    }
+    getRandomPosition(element: HTMLElement) {
+        return {
+            top: `${this.getRandomNumber(element.offsetHeight || 1000)}px`,
+            left: `${this.getRandomNumber(element.offsetWidth)}px`,
+        };
+    }
+    getRandomNumber(value: number) {
+        return Math.floor(Math.random() * value);
+    }
+    setProp1(value: string) {
+        this.setProperty("prop1", value, true);
+        return this;
+    }
+    setSrc1(value: string) {
+        this.setProperty("src1", value, true);
+        return this;
+    }
 }
